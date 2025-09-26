610d2041182f9393cb485580eb8e046666f204c7
maitreya-16 <maitreyavaidyam@gmail.com>
2025-09-26 19:42:27 +0530

    nginx-conf-added ^_^

--- server.js
+++ server.js
@@ -22,7 +22,11 @@ const cors = require("cors");
 // CORS configuration
 const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(',')
-  : ["http://localhost:5173", "http://localhost:3000"];
+  : [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://ctd-ncc-2025.vercel.app"
+    ];
 
 // Allow all origins
 app.use(cors({
